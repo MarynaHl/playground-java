4b58657ddef4107c5167310a6dad07282193a193
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-05-16 14:29:54 +0100

    implement Requirements methods

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-        // Тут пиши свої команди
-String name = "Mary";
-int age = 3;
-boolean really = false;
-System.out.println("My name is " + name + " and im " + age + " years old! " + "But it is " + really );
-
+        Requirements.requireNonNull("hello", "Object cannot be null");
+        Requirements.checkArgument(true, "Must be true");
     }
 }
