fe0b0c0ced9db7d12a658b3c1aa62acf240a18d0
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-05-24 12:00:46 +0100

    implement Requirements methods

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
-import java.util.List;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("\n\tjava " + 16);
+    public static void main(String[] args) {
+        int x;
+        try {
+            x = System.in.read(); // Зчитує один символ з клавіатури
+            System.out.println("code = " + x + " x = " + (char)x);
+        } catch (IOException e) {
+            e.printStackTrace(); // Виводить помилку, якщо вона сталася
+        }
     }
-    }
-
+}
