4d189c8226073e9792668e215745729e1a2c9d14
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-05-25 21:31:43 +0100

    implement Requirements methods

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int secondsInput = scanner.nextInt();
-        int hours = (secondsInput/3600) % 24;
-        int minutes = (secondsInput/60) % 60;
-        int seconds = secondsInput % 60;
+        int secondInput = scanner.nextInt();
+        int hours = (secondInput/3600)%24;
+        int minutes = (secondInput/60)%60;
+        int seconds = secondInput%60;
 
         System.out.printf("%d:%02d:%02d", hours, minutes, seconds);
+
     }
 }
