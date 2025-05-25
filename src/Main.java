901ca6e29ba803ffd19217bcb999ba1b40adf586
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-05-25 16:38:15 +0100

    implement Requirements methods

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int experience = 15;
-        int requirements = 10;
-        String result = (experience > requirements) ? "Accept to projects" : "Learn more";
-        System.out.println(result);
+        Scanner scanner = new Scanner(System.in);
+        int secondsInput = scanner.nextInt();
+        int hours = (secondsInput/3600) % 24;
+        int minutes = (secondsInput/60) % 60;
+        int seconds = secondsInput % 60;
+
+        System.out.printf("%d:%02d:%02d", hours, minutes, seconds);
     }
 }
