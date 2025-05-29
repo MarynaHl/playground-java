72c42059271a2fe7613e030969b76944dbdcfc00
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-05-29 16:00:50 +0100

    implement Requirements methods

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,12 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int bill = scanner.nextInt();
-        if (bill < 0) {
-            System.out.println("Bill total amount cannot be negative");
-            return;
+        int[] [] spiral = Spiral.spiral(3, 4);
+        for (int[] row : spiral) {
+            for (int col : row) {
+                System.out.print(String.format("%4d", col));
+            }
+            System.out.println();
         }
-        int friends = scanner.nextInt();
-        if (friends <= 0) {
-            System.out.println("Number of friends cannot be negative");
-            return;
-        }
-
-        int totalWithTip = bill + bill / 10;
-        int partToPay = totalWithTip / friends;
-
-        System.out.println(partToPay);
     }
+
 }
